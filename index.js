e890daca76f87bd8ec32b628c75346adf871e94d
Noah <noahschnur@gmail.com>
2021-06-01 13:46:58 -0700

    add dept and role

--- index.js
+++ index.js
@@ -1,78 +1,79 @@
-const inquirer = require('inquirer');
-require('console.table');
-const connection = require("./db/connection")
-
+const inquirer = require("inquirer");
+require("console.table");
+const connection = require("./db/connection");
 
 function runTracker() {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'start',
-            message: "What would you like to do?",
-            choices: ["View all employees", "View employees by department", "View employees by manager", "Add employee", "Remove employee", "Update employee role", "Update employee manager"],
-        }
-    ]).then((answers) => {
-        switch(answers.start) {
-            case 'View all employees': 
-                viewEmployees()
-                break;
-            case 'View employees by department':
-                viewDepartment()
-                break;
-                }
-    })
-};
-
-
-function viewEmployees() {
-let query= `SELECT employees.id, employees.first_name, employees.last_name, employees.role_id, employees.manager_id FROM employee_db.employees`
-
-connection.query(query, (err, res)=>{
-    if (err) throw err
-    console.log("all the employees")
-    console.table(res)
-    runTracker();
-})
-
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "start",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update employee role",
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.start) {
+        case "View all departments":
+          viewDepartment();
+          break;
+        case "View all roles":
+          viewRole();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update employee role":
+          updateRole();
+          break;
+      }
+    });
 }
 
-// if View all employees by dept
-//     db.query(`SELECT * FROM employees WHERE department_id =?`, (err, values) => {
-    // if (err) {
-    //     console.log(err);
-    // }
-    // console.table([first_name, last_name], values)
-// });
+function viewDepartment() {
+  let query = `SELECT * FROM employee_db.department`;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.log("all the employees");
+    console.table(res);
+    runTracker();
+  });
+}
 
-// if View all employees by manager
-//     db.query(`SELECT * FROM employees WHERE manager_id =?`, (err, values) => {
-    // if (err) {
-    //     console.log(err);
-    // }
-    // console.table([first_name, last_name], values)
-// });
+function viewRole() {
+    let query = `SELECT * FROM employee_db.role`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        runTracker();
+    });
+}
 
-// if Add employees
-//     db.query(`INSERT INTO employees(id, first_name, last_name, role_id, manager_id) VALUES (?,?,?,?,?)`
-// console.table([
-// ]);
+function viewEmployees() {}
 
-//// if Remove an employee
-//     db.query(`DELETE FROM employees WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-// console.log(result);
-// ]);
+function addDepartment() {}
 
-// if UPDATE employee role
-// console.table([
-//     `INSERT INTO employees(id, role_id)`
-// ]);
+function addRole() {}
 
-// if UPDATE employee manager
-// console.table([
-//     `INSERT INTO employees(id, manager_id)`
-// ]);
+function addEmployee() {}
+
+function updateRole() {}
 
 runTracker();
